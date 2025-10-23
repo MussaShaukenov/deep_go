6e8606797c2e3dbb269fdc4da162f37247514250
Mussa <mshaukenov@gmail.com>
2025-10-23 18:51:32 +0500

    hw1: change to byte implementation

--- hw1/main.go
+++ hw1/main.go
@@ -2,27 +2,15 @@ package main
 
 import (
 	"fmt"
-	"strconv"
-	"strings"
 )
 
 // go test -v homework_test.go
 
 func ToLittleEndian(number uint32) uint32 {
-	s := fmt.Sprintf("%032b", number) // pad to 32 bits
-	bytes := []string{}
-	for i := 0; i < 32; i += 8 {
-		bytes = append(bytes, s[i:i+8])
-	}
-
-	// reverse byte order
-	for i, j := 0, len(bytes)-1; i < j; i, j = i+1, j-1 {
-		bytes[i], bytes[j] = bytes[j], bytes[i]
-	}
-
-	result := strings.Join(bytes, "")
-	endResult, _ := strconv.ParseUint(result, 2, 32)
-	return uint32(endResult)
+	return (number>>24)&0xFF |
+		((number >> 8) & 0xFF00) |
+		((number << 8) & 0xFF0000) |
+		((number << 24) & 0xFF000000)
 }
 
 func main() {
